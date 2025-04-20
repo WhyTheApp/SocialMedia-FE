d0a5e2cb638731d2548d2101326ca88137d4a6da
denisdenis05 <66214092+denisdenis05@users.noreply.github.com>
2025-04-20 23:08:09 +0300

    Fixed placeholders

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
 export const JoinText = "Join us";
 export const AppTitle = "why - the app";
-export const ThankYouText = "Thank you.";
+export const ThankYouText =
+  "We will keep you updated.\nThank you for your support";
+export const EmailText = "Email";
+export const FeedbackText = "Anything else you wish from the app";
